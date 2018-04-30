d7669cec9d89306135406e65f33327e8dd090658
Candy <229355694@qq.com>
2018-04-30 21:19:01 +0800

    finish practice 5

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -21,6 +21,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //4.实现对Employee的分页查询，每页两个数据
 
     //5.查找**的所在的公司的公司名称
+    @Query(value = "select c.companyName from Company c,Employee e where e.name = ?1 " +
+            "and e.companyId = c.id",nativeQuery = true)
+    String findCompanyNameByName(String name);
 
     //6.将*的名字改成*,输出这次修改影响的行数
     @Query(value = "update Employee e set e.name=?1 where e.name=?2 ", nativeQuery = true)
